42ba3c1f14a0c2b88f759e60982ee2508ba93248
cpnsbpbddki <cpns.bpbddki@gmail.com>
2026-02-15 00:55:49 +0700

    Delete worker.js

--- worker.js
+++
@@ -1,8 +0,0 @@
-name = "EMAILS"
-main = "src/worker.js"
-compatibility_date = "2026-01-14"
-
-# Binding KV (Ganti ID dengan ID KV Namespace Anda)
-[[kv_namespaces]]
-binding = "EMAILS"
-id = "a964fdd8653e4a4384b4d584f3319d2c"
